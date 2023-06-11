d596c143fc1e6f6beb93b235b2aeb7811abda6cb
Vako2179 <vakoxm7@gmail.com>
2023-06-11 15:15:19 +0200

    commit for cesar sipher

---
+++ main.py
@@ -0,0 +1,38 @@
+from art import cifer
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v',
+            'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
+            'r',
+            's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n',
+            'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+print(cifer)
+
+
+def cipher(start_text, shift_amount, cipher_direction):
+    end_text = ""
+    if cipher_direction == "decode":
+        shift_amount *= -1
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
+    print(f"The {cipher_direction}d text is: {end_text}")
+
+
+while True:
+    encode_or_decode = input("Welcome to the Caesar cipher. Type 'encode' to encrypt or 'decode' to decrypt: ")
+    type_for_encode = input("Type your message: ")
+    shift_number = int(input("Type the shift number: "))
+    shift_number = shift_number % 26
+
+    print(cipher(start_text=type_for_encode, shift_amount=shift_number, cipher_direction=encode_or_decode))
+
+    continue_choice = input("Do you want to continue? Type 'yes' to continue or any other key to exit: ")
+    if continue_choice != 'yes':
+        break
